6510cfd04fd8f3e535966d0015e144af4e106272
Rafael Warnault <nark3411@free.fr>
2011-12-27 12:24:40 +0100

    try to fix submodule issue

--- config.h
+++ config.h
@@ -92,7 +92,7 @@
 #define WD_VERSION "2.0"
 
 /* Subversion revision */
-#define WI_REVISION 0x0e71865
+#define WI_REVISION 0xe7ce035
 
 /* 64-bit ino_t on Darwin */
 #define _DARWIN_USE_64_BIT_INODE 1
