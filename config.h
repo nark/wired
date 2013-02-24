b99c4854a0e2582843ac73dbd11e7b848c2c9760
Rafaël Warnault <rw@read-write.fr>
2013-02-24 23:53:50 +0100

    Add new remote

--- config.h
+++ config.h
@@ -92,7 +92,7 @@
 #define WD_VERSION "2.0"
 
 /* Subversion revision */
-#define WI_REVISION "e2a6c3a"
+#define WI_REVISION "f9e0eea"
 
 /* 64-bit ino_t on Darwin */
 #define _DARWIN_USE_64_BIT_INODE 1
